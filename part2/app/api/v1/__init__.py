16a293469bd412b5ce9c90e8e3d21c9abd2e9995
GerAle30 <garciaslj@gmail.com>
2025-06-22 01:36:15 -0400

    Refactor API initialization and fix BaseModel class definition

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,1 +1,11 @@
-sgundo init
+from flask_restx import Api
+from flask import Blueprint
+
+from .user import api as user_ns
+
+blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+api = Api(blueprint, version='1.0', title='HBnB API',
+          description='API for HBnB app')
+
+# Register namespaces
+api.add_namespace(user_ns, path='/users')
--- part2/app/models/base_models.py
+++ part2/app/models/base_models.py
@@ -1,22 +1,20 @@
-# app/models/base.py
-
 import uuid
 from datetime import datetime
 
 
-class baseModel:
+class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4()))
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
         def save(self):
-            """Update the time stamp whenever the object"""
-            self.updated_at = date time.now()
+            # Update the update_at timestamp whenever the object is modify
+            self.updated_at = datetime.now()
 
             def update(self, data):
-                """Update using a dictionary"""
+                # Update the attributes of the object based on the provided dic
                 for key, value in data.items():
                     if hasattr(self, key):
                         setattr(self, key, value)
-                        self.save() 
+                self.save()   # Update the updated_at timestamp
