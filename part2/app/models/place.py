eaa6de0936d9fb4ce93b0697f90035fb0572bfc7
GerAle30 <garciaslj@gmail.com>
2025-06-23 01:50:32 -0400

    Implement CRUD operations for amenities, places, reviews, and users with input validation and error handling

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,1 +1,23 @@
-second place in models part
+from .base_model import BaseModel
+
+
+class Place(BaseModel):
+    def __init__(self, name="", description="", price=0, latitude=0.0,
+                 longitude=0.0, owner=None):
+        super().__init__()
+        self.name = name
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner  # user object ref
+        self.reviews = []  # least reviews object
+        self.amenities = []  # least amen object
+
+    def add_reviews(self, review):
+        if review and review.place == self:
+            self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        if amenity not in self.amenities:
+            self.amenities.append(amenity)
--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,1 +1,25 @@
-second user in models part
+# In (app) settings/models/user.py
+
+
+from app.models.base import BaseModel
+import re
+
+
+class User(BaseModel):
+    EMAIL_REGEX = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        if not first_name or len(first_name) > 50:
+            raise ValueError(
+                    "First name is required  must be <= 50 characters.")
+        if not last_name or len(last_name) > 50:
+            raise ValueError("
+                             Last name is required  must be <= 50 characters.")
+        if not email or not re.match(self.EMAIL_REGEX, email):
+            raise ValueError("A valid email is required.")
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
