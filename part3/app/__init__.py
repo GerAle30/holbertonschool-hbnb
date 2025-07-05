2340e4e20cbb502a0f071a622e3943a57ed3c2d6
GerAle30 <garciaslj@gmail.com>
2025-07-05 18:04:54 -0400

    mensaje

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -8,6 +8,9 @@ bcrypt = Bcrypt()
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    # Initialize Bcrypt
+    bcrypt.init_app(app)
 
     # Register API blueprint
     from app.api.v1 import blueprint as api_v1
