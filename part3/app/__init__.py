bd678a2b4b4eb279720be801a1a2e35878794f91
GerAle30 <garciaslj@gmail.com>
2025-07-06 18:43:27 -0400

    mensaje

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -11,6 +13,9 @@ def create_app(config_class="config.DevelopmentConfig"):
     
     # Initialize Bcrypt
     bcrypt.init_app(app)
+    
+    # Initialize JWT
+    jwt.init_app(app)
 
     # Register API blueprint
     from app.api.v1 import blueprint as api_v1
